e7587e97173be76bb316bdcb11667e1a3ca0d075
datchanhkun <caothanhdat.org@gmail.com>
2021-09-03 03:48:27 +0700

    Design Database Schema & Manage mongodb collection

---
+++ src/models/card.model.js
@@ -0,0 +1,34 @@
+import Joi from 'joi'
+import { getDB } from '*/config/mongodb'
+//Định nghĩa 'card' collection
+const cardCollectionName = 'cards'
+const cardCollectionSchema = Joi.object({
+  boardId: Joi.string().required(),
+  columnId: Joi.string().required(),
+  title: Joi.string().required().min(3).max(20),
+  cover: Joi.string().default(null),
+  createdAt: Joi.date().timestamp().default(Date.now()),
+  updatedAt: Joi.date().timestamp().default(null),
+  _destroy: Joi.boolean().default(false)
+})
+
+//Sử dụng abortEarly để hiển thị đầy đủ lỗi -> mặc định chỉ hiển thị line đầu tiên rồi dừng
+const validateSchema = async (data) => {
+  return await cardCollectionSchema.validateAsync(data, { abortEarly: false })
+}
+
+const createNew = async (data) => {
+  try {
+    //data đã được validate
+    const value = await validateSchema(data)
+    //Await đến hàm GetDB rồi insert cái value đã validate vào
+    const result = await getDB().collection(cardCollectionName).insertOne(value)
+    return await getDB().collection(cardCollectionName).findOne(result.insertedId)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const CardModel = {
+  createNew
+}
--- src/server.js
+++ src/server.js
@@ -1,16 +1,30 @@
 import express from 'express'
 import { connectDB } from '*/config/mongodb'
 import { env } from '*/config/environment'
-
-const app = express()
+import { BoardModel } from '*/models/board.model'
 
 //Connect DB
-connectDB().catch(console.log)
-app.get('/', (req, res) => {
-  res.end('test nodejs')
-})
+connectDB()
+  .then(() => console.log('Connected to MongoDB Server Successfully!'))
+  .then(() => bootServer())
+  .catch(error => {
+    console.log(error)
+    process.exit(1) //crash app
+  })
 
-app.listen(env.PORT, env.HOST, () => {
-  console.log(`App is running at ${env.HOST}:${env.PORT}`)
-})
+const bootServer = () => {
+  const app = express()
 
+  app.get('/test', async (req, res) => {
+    let fakeData = {
+      title: 'board-1'
+    }
+    const newBoard = await BoardModel.createNew(fakeData)
+    console.log(newBoard)
+    res.end('test nodejs')
+  })
+
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`App is running at ${env.APP_HOST}:${env.APP_PORT}`)
+  })
+}
